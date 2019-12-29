14bebf466a573b791f6be435c4fea3d7186c9370
Atanas Vasilev <nasko01vasilev@gmail.com>
2019-12-29 22:04:39 +0200

    Fast property getters using Expression Trees

---
+++ ExpressionTrees/PropertyGettersWithExpressionTrees/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace PropertyGettersWithExpressionTrees
+{
+    class Program
+    {
+        private const int OperationsCount = 100000;
+
+        static void Main(string[] args)
+        {
+            //return RedirectToAction("Index", new { id = 10, query = "Fast property getters using expression trees" }
+            // Dictionary { ["id"] = 10, ["query"] = "Fast property getters using expression trees" }
+
+            var queryParameters = new { id = 10, query = "Fast property getters using expression trees" };
+            var queryParametersDictionary = new Dictionary<string, object>();
+            var stopwatch = Stopwatch.StartNew();
+
+            for (int i = 0; i < OperationsCount; i++)
+            {
+                queryParameters
+                    .GetType()
+                    .GetProperties()
+                    .Select(property => new
+                    {
+                        property.Name,
+                        Value = property.GetValue(queryParameters)
+                    })
+                    .ToList()
+                    .ForEach(property => queryParametersDictionary[property.Name] = property.Value);
+            }
+
+            Console.WriteLine(queryParametersDictionary.Count);
+            Console.WriteLine($"{stopwatch.Elapsed} - Reflection property getters");
+
+            //If it is precached it is even faster beacuse the only slow method is compilation of the lambda function
+            PropertyHelper.GetProperties(queryParameters);
+            stopwatch = Stopwatch.StartNew();
+
+            for (int i = 0; i < OperationsCount; i++)
+            {
+                PropertyHelper
+                   .GetProperties(queryParameters)
+                   .ForEach(property => queryParametersDictionary[property.Name] = property.Value);
+            }
+
+            Console.WriteLine(queryParametersDictionary.Count);
+            Console.WriteLine($"{stopwatch.Elapsed} - Expression trees property getters");
+        }
+    }
+}
---
+++ ExpressionTrees/PropertyGettersWithExpressionTrees/PropertyHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace PropertyGettersWithExpressionTrees
+{
+    public class PropertyHelper
+    {
+        private static readonly Type typeOfObject = typeof(object);
+        private static readonly ConcurrentDictionary<Type, List<PropertyHelper>> cache = new ConcurrentDictionary<Type, List<PropertyHelper>>();
+
+        public string Name { get; set; }
+
+        public object Value { get; set; }
+
+        public static List<PropertyHelper> GetProperties(object insatnce)
+        {
+            Type instanceType = insatnce.GetType();
+            return cache.GetOrAdd(instanceType, _ =>
+            {
+                List<PropertyHelper> properties = new List<PropertyHelper>();
+
+                foreach (PropertyInfo property in instanceType.GetProperties())
+                {
+                    // object obj
+                    ParameterExpression parameter = Expression.Parameter(typeOfObject, "obj");
+                    // (T)obj
+                    UnaryExpression convertedParameter = Expression.Convert(parameter, instanceType);
+                    // ((T)obj).Property
+                    MemberExpression body = Expression.MakeMemberAccess(convertedParameter, property);
+                    // (object)((T)obj).Property
+                    UnaryExpression convertedProperty = Expression.Convert(body, typeOfObject);
+                    // object obj => (object)((T)obj).Property
+                    Expression<Func<object, object>> lambdaExpression = Expression.Lambda<Func<object, object>>(convertedProperty, parameter);
+                    
+                    Func<object, object> propetyGetterFunc = lambdaExpression.Compile();
+
+                    properties.Add(new PropertyHelper
+                    {
+                        Name = property.Name,
+                        Value = propetyGetterFunc.Invoke(insatnce)
+                    });
+                }
+
+                return properties;
+            });
+        }
+    }
+}
