67e0777d97027b89deca8457d39c4ffa4cef61c8
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-03-10 22:30:00 +0200

    LINQ Multiple enumeration

---
+++ CSharpTipsAndTricks/Traps/MultipleEnumeration/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace MultipleEnumeration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==== Multiple enumerations:");
+            LinqQueryMultipleEnumeration();
+
+            Console.WriteLine("==== Single enumeration (calling .ToList()):");
+            LinqQuerySingleEnumeration();
+        }
+
+        public static void LinqQueryMultipleEnumeration()
+        {
+            var results = Enumerable.Range(1, 3).Select(x => new { Number = x, Time = DateTime.Now.ToString("O") });
+
+            Console.WriteLine("First enumeration:");
+            foreach (var result in results)
+            {
+                Console.WriteLine(result);
+            }
+
+            Console.WriteLine("Second enumeration:");
+            foreach (var result in results)
+            {
+                Console.WriteLine(result);
+            }
+        }
+
+        public static void LinqQuerySingleEnumeration()
+        {
+            var results = Enumerable.Range(1, 3).Select(x => new { Number = x, Time = DateTime.Now.ToString("O") }).ToList();
+
+            Console.WriteLine("First enumeration:");
+            foreach (var result in results)
+            {
+                Console.WriteLine(result);
+            }
+
+            Console.WriteLine("Second enumeration:");
+            foreach (var result in results)
+            {
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
