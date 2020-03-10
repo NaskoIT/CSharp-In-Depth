ec6b8bd89fc8b734476b01ce49ee4fa2a9cf054d
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-03-10 16:56:30 +0200

    String references

---
+++ CSharpTipsAndTricks/Traps/StringReferences/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StringReferences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var string1 = "some value";
+            var string2 = "some value";
+            Console.WriteLine("ReferenceEquals(string1, string2) = {0}", ReferenceEquals(string1, string2)); // true
+            Console.Write("Please enter \"some value\": ");
+            var stringFromConsole = Console.ReadLine();
+            Console.WriteLine("ReferenceEquals(string1, stringFromConsole) = {0}", ReferenceEquals(string1, stringFromConsole)); // false
+            var stringInternFromConsole = string.Intern(stringFromConsole);
+            Console.WriteLine("ReferenceEquals(string1, stringInternFromConsole) = {0}", ReferenceEquals(string1, stringInternFromConsole)); //true
+        }
+    }
+}
