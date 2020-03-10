6221558b331808ec0e2cfe3e725147e8b0a87a45
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-03-10 17:12:54 +0200

    Preserving stack trace when re-throw exception

---
+++ CSharpTipsAndTricks/Traps/PreservingStackTraceWhenRethrowingExceptions/ExceptionThrower.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace PreservingStackTraceWhenRethrowingExceptions
+{
+    public class ExceptionThrower
+    {
+        public void WithoutStacktrace()
+        {
+            try
+            {
+                this.FirstMethod();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("==================== WithoutStacktrace() ====== throw ex;");
+                throw ex;
+            }
+        }
+
+        public void WithStacktrace()
+        {
+            try
+            {
+                this.FirstMethod();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("==================== WithStacktrace() ====== throw;");
+                throw;
+            }
+        }
+
+        public void WithNewException()
+        {
+            try
+            {
+                this.FirstMethod();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("==================== WithNewException() ====== throw new Exception();");
+                throw new Exception();
+            }
+        }
+
+        private void FirstMethod()
+        {
+            this.SecondMethod();
+        }
+
+        private void SecondMethod()
+        {
+            this.ThirdMethod();
+        }
+
+        private void ThirdMethod()
+        {
+            throw new Exception("Exception from ThirdMethod()");
+        }
+    }
+}
---
+++ CSharpTipsAndTricks/Traps/PreservingStackTraceWhenRethrowingExceptions/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PreservingStackTraceWhenRethrowingExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Notice TargetSite and StackTrace
+            var exceptionThrower = new ExceptionThrower();
+            try
+            {
+                exceptionThrower.WithoutStacktrace();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            try
+            {
+                exceptionThrower.WithStacktrace();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            try
+            {
+                exceptionThrower.WithNewException();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
