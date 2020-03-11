3e04c615225295efcd67bb2a418f47a123e9f399
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-03-11 09:51:13 +0200

    Casting vs as operator

---
+++ CSharpTipsAndTricks/SyntacticSugar/CastingVsAsOperator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CastingVsAsOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object number = "Five";
+
+            Console.WriteLine("The type of 'number' is: {0}", number.GetType());
+
+            if (number is string)
+            {
+                Console.WriteLine("'number is string' is true");
+            }
+
+            //// This will cause an unhandled exception of type 'InvalidCastException' because the specified cast is not valid.
+            // var numberAsInt = (int?)number;
+
+            var numberAsInt = number as int?;
+            //// This will cause an unhandled exception of type 'InvalidOperationException' because numberAsInt will be null
+            // Console.WriteLine(numberAsInt.Value);
+
+            //// When using the 'as' operator we should always consider the possible null value
+            Console.WriteLine(numberAsInt.GetValueOrDefault());
+        }
+    }
+}
