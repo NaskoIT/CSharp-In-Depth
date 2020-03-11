58018a27a53bba278e70896dc38dccbe114addda
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-03-11 17:36:07 +0200

    Optional parameters

---
+++ CSharpTipsAndTricks/SyntacticSugar/OptionalParameters/OptionalParameters.Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using OptionalParameters.Library;
+using System;
+
+namespace OptionalParameters.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bankAccount = new BankAccount("John Doe");
+            Console.WriteLine(bankAccount);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharpTipsAndTricks/SyntacticSugar/OptionalParameters/OptionalParameters.Library/BankAccount.cs
@@ -0,0 +1,22 @@
+﻿namespace OptionalParameters.Library
+{
+    public class BankAccount
+    {
+        // Try changing the default value of the money parameter without pre-compiling the client
+        // Try adding new optional parameter without pre-compiling the client
+        public BankAccount(string accountHolder = default, decimal money = 1000)
+        {
+            this.AccountHolder = accountHolder;
+            this.Money = money;
+        }
+
+        public string AccountHolder { get; set; }
+
+        public decimal Money { get; set; }
+
+        public override string ToString()
+        {
+            return $"Bank account of {this.AccountHolder} with {this.Money} in cash.";
+        }
+    }
+}
