4227bb005373f4f618a95314d9aae1857f0e84bd
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-03-11 17:52:48 +0200

    Yield

---
+++ CSharpTipsAndTricks/Yield/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Yield
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var evenNumber in YieldNumbersGenerator.EvenNumbers(50, 60))
+            {
+                Console.WriteLine("Iterated number {0}", evenNumber);
+            }
+        }
+    }
+}
---
+++ CSharpTipsAndTricks/Yield/YieldNumbersGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Yield
+{
+    public static class YieldNumbersGenerator
+    {
+        public static IEnumerable<int> EvenNumbers(int from, int to)
+        {
+            for (var i = from; i <= to; i++)
+            {
+                Console.WriteLine("Processing number {0}", i);
+                if (i % 2 == 0)
+                {
+                    yield return i; // yield break allow us to stop yielding
+                }
+            }
+        }
+    }
+}
