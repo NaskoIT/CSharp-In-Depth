f80747cc021f27fff0a2baf4b1ea5aa2840638b4
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-03-12 11:23:34 +0200

    Method deprecation

---
+++ CSharpTipsAndTricks/UsefulClassesAndMethods/MethodDeprecation/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace MethodDeprecation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xmlReaderFactory = new XmlReaderFactory();
+            xmlReaderFactory.CreateXmlReader();
+
+            // Compilation warning:
+            // xmlReaderFactory.CreateXml();
+
+            // Compilation error:
+            // xmlReaderFactory.Create();
+        }
+    }
+}
---
+++ CSharpTipsAndTricks/UsefulClassesAndMethods/MethodDeprecation/XmlReaderFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MethodDeprecation
+{
+    public class XmlReaderFactory
+    {
+        [Obsolete("CreateXml() method is deprecated. Use CreateXmlReader instead.", false)]
+        public void CreateXml()
+        {
+        }
+
+        // Using this method will cause compilation error
+        [Obsolete("Create() method is deprecated. Use CreateXmlReader instead.", true)]
+        public void Create()
+        {
+        }
+
+        public void CreateXmlReader()
+        {
+        }
+    }
+}
