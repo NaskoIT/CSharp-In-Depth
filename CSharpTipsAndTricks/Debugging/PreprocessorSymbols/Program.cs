391365d0ef606daba5f240c1c70b18b10758095b
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-03-12 13:50:39 +0200

    Preprocessor Symbols

---
+++ CSharpTipsAndTricks/Debugging/PreprocessorSymbols/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace PreprocessorSymbols
+{
+    public class Program
+    {
+        public static void Main()
+        {
+#if DEBUG
+            Console.WriteLine("DEBUG");
+#elif CI_BUILD
+            Console.WriteLine("CI_BUILD");
+#else
+            Console.WriteLine("Neither DEBUG nor CI_BUILD");
+#error Neither DEBUG nor CI_BUILD
+#endif
+            CallOnlyInDebug();
+
+            Debug.WriteLine("Hello, its me");
+        }
+
+        [Conditional("DEBUG")]
+        private static void CallOnlyInDebug()
+        {
+            Console.WriteLine("CallOnlyInDebug() called");
+        }
+    }
+}
