42b43b2a48b44c4db30e6a8a782c64132125fbbf
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-04-05 00:52:45 +0300

    Real live example with burgers cooking

---
+++ AsynchronousProgramming/BurgerPreparation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace BurgerPreparation
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine(nameof(SyncCooker));
+            SyncCooker.Work();
+
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine(nameof(AllAtOnceCooker));
+            await AllAtOnceCooker.Work();
+
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine(nameof(AsyncCooker));
+            await AsyncCooker.Work();
+        }
+    }
+}
