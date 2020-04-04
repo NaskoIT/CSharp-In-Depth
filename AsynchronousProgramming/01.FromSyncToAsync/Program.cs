9dd175bb2ecd0e3d923246792267e4a91e701b84
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-04-05 00:35:17 +0300

    Some simple examples

---
+++ AsynchronousProgramming/01.FromSyncToAsync/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace FromSyncToAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simple example");
+            SimpleExample();
+
+            Console.WriteLine(new string('-', 50));
+
+            Console.WriteLine("Complex exmple");
+            ComplexExample();
+        }
+
+        public static void SimpleExample()
+        {
+            var task = Task.Run(() => Console.WriteLine("First"));
+
+            Console.WriteLine("Second");
+
+            task.Wait();
+        }
+
+        public static void ComplexExample()
+        {
+            Task<string> task = Task.Run(() => Task
+                    .Delay(2000)
+                    .ContinueWith(t => "In a task"));
+
+            Task.Delay(4000).Wait();
+            Console.WriteLine("Outside of a task!");
+
+            Task<Task> completion = Task
+                .WhenAll(task)
+                .ContinueWith(async t =>
+                {
+                    Console.WriteLine((await t)[0]);
+                });
+
+            completion.Wait();
+        }
+    }
+}
