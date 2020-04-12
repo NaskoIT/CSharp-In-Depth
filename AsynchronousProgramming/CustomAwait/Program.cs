6384ecbfe2333d88160b61220d28384d3c664eef
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-04-12 18:21:13 +0300

    Custom Await

---
+++ AsynchronousProgramming/CustomAwait/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CustomAwait
+{
+    public class Program
+    {
+        public static async Task Main()
+        {
+            var content = await new Uri("https://github.com/NaskoVasilev");
+            Console.WriteLine(content);
+
+            await TimeSpan.FromSeconds(3);
+
+            await 1000; // In milliseconds.
+
+            await new List<Task>
+            {
+                Task.Delay(1000),
+                Task.Delay(2000)
+            };
+        }
+    }
+}
