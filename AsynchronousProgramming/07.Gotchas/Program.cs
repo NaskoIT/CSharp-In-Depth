9fbff31596a1b277f13edec64d7a5a0dcd447ad3
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-04-12 16:40:53 +0300

    Antipatterns  and Cancelation

---
+++ AsynchronousProgramming/07.Gotchas/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Gotchas
+{
+    public class Program
+    {
+        public static async Task Main()
+        {
+            // Remember: async void is fire and forget. It is really bad pracice to be used!!!
+            RunAsyncVoid();
+
+            // AsyncVoidLambda();
+
+            // NestedTasks();
+
+            // Uncomment the line below and see the differene
+            // Console.ReadLine();
+        }
+
+        public static void RunAsyncVoid()
+        {
+            try
+            {
+                AsyncVoid();
+            }
+            catch
+            {
+                Console.WriteLine("Cannot be caught!");
+            }
+        }
+
+        public static void AsyncVoidLambda()
+        {
+            try
+            {
+                var list = new List<int> { 1, 2, 3, 4, 5 };
+
+                list.ForEach(async number =>
+                {
+                    await Task.Run(() => Console.WriteLine(number));
+
+                    throw new InvalidOperationException("In a lambda!");
+                });
+            }
+            catch
+            {
+                Console.WriteLine("Cannot be caught!");
+            }
+        }
+
+        public static async void AsyncVoid()
+        {
+            await Task.Run(() => Console.WriteLine("Message"));
+
+            throw new InvalidOperationException("Error");
+        }
+
+        public static async void NestedTasks()
+        {
+            await Task.Run(async () =>
+            {
+                Console.WriteLine("Before Delay");
+
+                await Task.Delay(1000);
+
+                Console.WriteLine("After Delay");
+            });
+
+            Console.WriteLine("After Task");
+        }
+    }
+}
---
+++ AsynchronousProgramming/09. Cancellation/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cancellation
+{
+    public class Program
+    {
+        public static async Task Main()
+        {
+            // var cancellation = new CancellationTokenSource(2000);
+            CancellationTokenSource cancellation = new CancellationTokenSource();
+
+            Task printTask = Task.Run(async () =>
+            {
+                while (true)
+                {
+                    if (cancellation.IsCancellationRequested)
+                    {
+                        Console.WriteLine("Enough!");
+
+                        // cancellation.Token.ThrowIfCancellationRequested();
+
+                        break;
+                    }
+
+                    Console.WriteLine(DateTime.Now);
+
+                    await Task.Delay(1000);
+                }
+            }, cancellation.Token);
+
+            Task readTask = Task.Run(() =>
+            {
+                while (true)
+                {
+                    string input = Console.ReadLine();
+
+                    if (input == "end")
+                    {
+                        cancellation.Cancel();
+                        break;
+                    }
+                }
+            });
+
+            await Task.WhenAll(printTask, readTask);
+        }
+    }
+}
