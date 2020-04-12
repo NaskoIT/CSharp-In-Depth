713edd91e8df5f9fc0e55f0a386d9d37a7b8535b
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-04-12 18:33:06 +0300

    Child Tasks - attached and detached demo

---
+++ AsynchronousProgramming/11.ChildTasks/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ChildTasks
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Detached();
+            //Attached();
+
+            //await DetachedAsync();
+        }
+
+        public static void Detached()
+        {
+            Task.Factory
+                .StartNew(() =>
+                {
+                    Console.WriteLine("First task detached");
+
+                    Task.Factory.StartNew(() =>
+                    {
+                        Task.Delay(10000).Wait();
+                        Console.WriteLine("Second task detached");
+                    });
+                })
+                .Wait();
+        }
+
+        public static void Attached()
+        {
+            Task.Factory
+                .StartNew(() =>
+                {
+                    Console.WriteLine("First task attached");
+
+                    Task.Factory.StartNew(() =>
+                    {
+                        Task.Delay(2000).Wait();
+                        Console.WriteLine("Second task attached");
+                    }, TaskCreationOptions.AttachedToParent);
+                })
+                .Wait();
+        }
+
+        private static async Task DetachedAsync()
+        {
+            await Task.Run(async () =>
+            {
+                Console.WriteLine("First task async");
+
+                await Task.Run(async () =>
+                {
+                    await Task.Delay(4000);
+                    Console.WriteLine("Second task async");
+                });
+            });
+        }
+    }
+}
