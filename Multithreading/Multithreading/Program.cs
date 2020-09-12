695f3d914f3ebe495112659a6679b69617407d7b
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-09-12 10:12:46 +0300

    Multithreading example

---
+++ Multithreading/Multithreading/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace Multithreading
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Thread thread = new Thread(PrintMessage);
+            thread.Start();
+            Console.WriteLine("Message from the maim thread");
+        }
+
+        public static void PrintMessage()
+        {
+            Console.WriteLine("This method will be executed in different thread");
+        }
+    }
+}
