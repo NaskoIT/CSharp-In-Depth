4aa20b83522fd904fc60c11c3f0036d3cb3cbf65
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-09-13 10:50:48 +0300

    Lock example

---
+++ Multithreading/Locks/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace RaceCondition
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            NumberGenerator numberGenerator = new NumberGenerator();
+            Thread[] threads = new Thread[5];
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(new ThreadStart(numberGenerator.Generate))
+                {
+                    Name = string.Format("Working Thread: {0}", i)
+                };
+            }
+
+            foreach (Thread thread in threads)
+            {
+                thread.Start();
+            }
+        }
+    }
+
+    public class NumberGenerator
+    {
+        private readonly object lockObject = new object();
+
+        public void Generate()
+        {
+            lock (lockObject)
+            {
+                Console.Write(" {0} is Executing", Thread.CurrentThread.Name);
+                for (int i = 0; i < 10; i++)
+                {
+                    Thread.Sleep(new Random().Next(5));
+                    Console.Write(" {0},", i);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
