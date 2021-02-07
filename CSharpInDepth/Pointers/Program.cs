3bddf32a0f7ef7b00001ccf1ad7e5b07f38dfe73
Atanas Vasilev <nasko01vasilev@gmail.com>
2021-02-07 22:51:54 +0200

    Show difference when pass a variable to a method by reference and by value

---
+++ CSharpInDepth/Pointers/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Pointers
+{
+    class Program
+    {
+        private static long topOfStack;
+        private static readonly long stackSize = 1024 * 1024;
+
+        unsafe static void Main(string[] args)
+        {
+            int someVariable = 5;
+            topOfStack = (long)&someVariable;
+
+            TestStruct test = new TestStruct
+            {
+                a = 123456789.123456789,
+                b = 123456789.123456789,
+                c = 123456789.123456789,
+                d = 123456789.123456789,
+                e = 123456789.123456789,
+                f = 123456789.123456789,
+                g = 123456789.123456789,
+                h = 123456789.123456789,
+            };
+
+            Console.WriteLine("Demo when we pass the parameter to the invoked function by value.");
+            RecursiveFunction(test);
+
+            Console.WriteLine(new string('-', 150));
+
+
+            Console.WriteLine("Demo when we pass the parameter to the invoked function by reference.");
+            RecursiveFunctionByReference(ref test);
+        }
+
+        unsafe private static void RecursiveFunction(TestStruct testStruct, int times = 0)
+        {
+            long usedStack;
+            usedStack = topOfStack - (long)&usedStack;
+            if (stackSize - usedStack < 0)
+            {
+                Console.WriteLine($"The function was invoked: {times} times before StackOverflow.");
+                return;
+            }
+
+            RecursiveFunction(testStruct, ++times);
+        }
+
+        unsafe private static void RecursiveFunctionByReference(ref TestStruct testStruct, int times = 0)
+        {
+            long usedStack;
+            usedStack = topOfStack - (long)&usedStack;
+            if (stackSize - usedStack < 0)
+            {
+                Console.WriteLine($"The function was invoked: {times} times before StackOverflow.");
+                return;
+            }
+
+            RecursiveFunctionByReference(ref testStruct, ++times);
+        }
+    }
+
+    struct TestStruct
+    {
+        public double a, b, c, d, e, f, g, h;
+    }
+}
