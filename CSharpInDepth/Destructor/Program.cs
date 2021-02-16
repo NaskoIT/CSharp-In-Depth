c277d76e742d7be9b5af9679c9426b31887a91f1
Atanas Vasilev <nasko01vasilev@gmail.com>
2021-02-16 23:59:34 +0200

    GC.Collect plus Deconstruct dmeo

---
+++ CSharpInDepth/Destructor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Destructor
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Details();
+            GC.Collect();
+            Console.WriteLine("GC.Collect was invoked!");
+        }
+
+        private static void Details()
+        {
+            var user = new User();
+        }
+    }
+
+    public class User
+    {
+        public User()
+        {
+            Console.WriteLine("An Instance of class created");
+        }
+
+        // Destructor
+        ~User()
+        {
+            Console.WriteLine("An Instance of class destroyed");
+        }
+    }
+}
