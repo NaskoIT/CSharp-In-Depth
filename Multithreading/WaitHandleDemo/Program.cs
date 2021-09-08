2729749ae15edddb2d1608b261b14169e6b2f82d
Atanas Vasilev <nasko01vasilev@gmail.com>
2021-09-08 17:08:50 +0300

    WaitHandle demo

---
+++ Multithreading/WaitHandleDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace WaitHandleDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ManualResetEvent[] events = new ManualResetEvent[10];
+
+            for (int i = 0; i < events.Length; i++)
+            {
+                events[i] = new ManualResetEvent(false);
+                Runner runner = new Runner(events[i], i);
+                new Thread(runner.Run).Start();
+            }
+
+            int index = WaitHandle.WaitAny(events);
+            Console.WriteLine($"**** The winner is: {index}");
+
+            WaitHandle.WaitAll(events);
+            Console.WriteLine("All finished!");
+        }
+    }
+}
---
+++ Multithreading/WaitHandleDemo/Runner.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace WaitHandleDemo
+{
+    public class Runner
+    {
+        private static readonly object obj = new object();
+        private static readonly Random random = new Random();
+        private readonly ManualResetEvent resetEvent;
+        private readonly int id;
+
+        public Runner(ManualResetEvent resetEvent, int id)
+        {
+            this.resetEvent = resetEvent;
+            this.id = id;
+        }
+
+        public void Run()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                int sleepTime;
+                lock (obj)
+                {
+                    sleepTime = random.Next(2000);
+                }
+
+                Thread.Sleep(sleepTime);
+                Console.WriteLine($"Runner {id} at stage {i}");
+            }
+
+            resetEvent.Set();
+        }
+    }
+}
