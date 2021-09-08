967dc500dcaf0e698bcf237d85585487c7c7533a
Atanas Vasilev <nasko01vasilev@gmail.com>
2021-09-08 15:38:48 +0300

    Thread start vs join example

---
+++ Multithreading/ThreadStartVsJoin/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadStartVsJoin
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Start printing the numbers");
+
+            var thread = new Thread(PrintNumbers);
+
+            thread.Start();
+
+            Console.WriteLine("Waiting for thread to finish work");
+
+            thread.Join();
+
+            Console.WriteLine("Work in the new thread is finshed!");
+        }
+
+        public static void PrintNumbers()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
