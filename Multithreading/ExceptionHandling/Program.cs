3c71faf88ea311f4d5a8de95dccef533e46803c1
Atanas Vasilev <nasko01vasilev@gmail.com>
2021-09-08 16:14:53 +0300

    Exception handling demo

---
+++ Multithreading/ExceptionHandling/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace ExceptionHandling
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                new Thread(DoWork).Start();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public static void DoWork()
+        {
+            throw new ArgumentException();
+        }
+    }
+}
